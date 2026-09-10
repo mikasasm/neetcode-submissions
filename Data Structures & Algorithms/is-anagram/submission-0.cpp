65c9b3ca267538cd4be08699f8f07b503fa495ec
mikasasm <mrinalsharma605@gmail.com>
2026-09-10 22:26:56 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int count[26]={};
+        for(int i =0;i<s.size();i++){
+            count[s[i]-'a']++;
+
+        }
+        for(int i=0;i<t.size();i++){
+            count[t[i]-'a']--;
+        }
+        for(int i=0; i<26;i++){
+            if(count[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
